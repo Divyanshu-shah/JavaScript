8ba3cc80eb9a75bac166aa9b5849672e3114ccf7
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-10 13:49:17 +0530

    update

---
+++ file1.js
@@ -0,0 +1,15 @@
+let a=10;
+let b=20;
+let c=a+b;
+console.log(c);
+console.log("Hello, World!");
+
+/*
+console.log("line1");
+console.log("line2");
+console.log("line3");
+*/
+
+// console.log("line1");
+// console.log("line2");
+// console.log("line3");
---
+++ file3.js
@@ -0,0 +1,31 @@
+let a=10;
+let b=4;
+
+// airthmetic operator
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+// comparison operator
+
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a==b);
+console.log(a!=b);
+
+let x=5;
+let y="5";
+console.log(x==y);
+console.log(x===y);
+
+// logical operator
+
+console.log(a>b && a>20);
+console.log(a>b || a>20);
+console.log(!(a>b));
