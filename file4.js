8b1d8294c1aef4e927f90dd5f7c38226ad285036
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-10 13:53:56 +0530

    updated

---
+++ file4.js
@@ -0,0 +1,13 @@
+let a=10;
+let b=5;
+
+// if(a>b){
+//     console.log("a is greater than b");
+// }else{
+//     console.log("b is greater than a");
+// }
+
+if(a>b)
+    console.log("a is greater than b");
+else
+    console.log("b is greater than a");
