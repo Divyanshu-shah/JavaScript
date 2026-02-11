43a1fb763c9313cf05c7f619fc2681e32c313c6a
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-11 16:42:47 +0530

    updated

---
+++ file15.js
@@ -0,0 +1,8 @@
+let cart = []
+// clicking on add to cart button on iphone
+cart = [...cart, "iphone"];
+console.log(cart);
+// clicking on add to cart button on speaker
+cart = [...cart, "speaker"];
+console.log(cart);
+
