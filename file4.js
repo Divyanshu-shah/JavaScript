ee7928c11675552423d8a4841df2a811efe25724
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-11 12:56:53 +0530

    updated

--- file4.js
+++ file4.js
@@ -1,11 +1,11 @@
 let a=10;
 let b=5;
 
-// if(a>b){
-//     console.log("a is greater than b");
-// }else{
-//     console.log("b is greater than a");
-// }
+if(a>b){
+    console.log("a is greater than b");
+}else{
+    console.log("b is greater than a");
+}
 
 if(a>b)
     console.log("a is greater than b");
--- file7.js
+++ file7.js
@@ -1,14 +1,16 @@
-// function greet() {
-//     console.log("Hello, welcome to JavaScript!");
-// }
-// greet();
-// greet();
+function greet() {
+    console.log("Hello, welcome to JavaScript!");
+}
+greet();
+greet();
 
-// function greet(name) {
-//     console.log("Hello " + name);
-// }
-// greet("Alice");
-// greet("Bob");
+
+function greet(name) {
+    console.log("Hello " + name);
+}
+greet("Alice");
+greet("Bob");
+
 
 function greet(name,place) {
     // console.log("Hello " + name + " from " + place);
