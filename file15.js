406c3d4501dc6f85927a56138a822f17d3dbb75d
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-11 21:43:57 +0530

    updated

--- file15.js
+++ file15.js
@@ -16,8 +16,63 @@ const order = {
 
 db.orders.insertOne(order);
 
+
 // create flipkart database
 // create orders collection > email, items, orderValue, status, orderDate
 // create user collection > name, email, password, role
 // Insert seed data
 // write aggregation query to display all the orders placed by a particular user
+
+
+
+// use flipkart
+
+// create orders collection
+
+db.orders.insertMany([
+  {
+    email: "Sam@gmail.com",
+    items: ["Keyboard"],
+    orderValue: 2000,
+    status: "Delivered",
+    orderDate: new Date("2025-02-05")
+  },
+  {
+    email: "john@gmail.com",
+    items: ["Mobile"],
+    orderValue: 30000,
+    status: "Pending",
+    orderDate: new Date("2025-02-03")
+  }
+])
+
+// create user collection
+
+db.users.insertMany([
+  {
+    name: "Sam",
+    email: "Sam@gmail.com",
+    password: "12345",
+    role: "customer"
+  },
+  {
+    name: "John",
+    email: "john@gmail.com",
+    password: "12345",
+    role: "customer"
+  }
+])
+
+// write aggregation query to display all the orders placed by a particular user
+
+db.orders.aggregate([
+  {
+    $match: { email: "Sam@gmail.com" }
+  }
+])
+
+db.orders.aggregate([
+  {
+    $match: { email: "john@gmail.com" }
+  }
+]) 
