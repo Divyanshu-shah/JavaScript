38edaa20f1461350107b8839ac0672c9e9c2744e
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-11 15:08:52 +0530

    ok

---
+++ file9.js
@@ -0,0 +1,28 @@
+// let a=10;
+// let name="John";
+// let flag=true;
+// let rating=4.5;
+// let score=null;
+// let city;
+// console.log(typeof a); // number
+// console.log(typeof name); // string 
+// console.log(typeof flag); // boolean
+// console.log(typeof rating); // number
+// console.log(typeof score); // object (null is considered an object in JavaScript)
+// console.log(typeof city); // undefined
+
+
+// let a="10";
+// console.log(typeof a); // string
+// // let num=parseInt(a);
+// let num=Number(a);
+// console.log(typeof num); // number
+
+
+let a="true";
+console.log(typeof a); // string
+let flag=Boolean(a);
+console.log(typeof flag); // boolean
+
+
+
