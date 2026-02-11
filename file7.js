2e9a37deb5ef7e8d099f93536a0853dbb6730c57
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-11 12:58:42 +0530

    updated

--- file7.js
+++ file7.js
@@ -1,21 +1,30 @@
-function greet() {
-    console.log("Hello, welcome to JavaScript!");
+// function greet() {
+//     console.log("Hello, welcome to JavaScript!");
+// }
+// greet();
+// greet();
+
+
+// function greet(name) {
+//     console.log("Hello " + name);
+// }
+// greet("Alice");
+// greet("Bob");
+
+
+// function greet(name,place) {
+//     // console.log("Hello " + name + " from " + place);
+//     console.log(`Hello ${name} from ${place}`);
+// }
+// greet("Alice","New York");
+// greet("Bob","London");
+
+
+// write a function to accept two numbers and print sum
+
+function sum(a,b) {
+    return a+b;
 }
-greet();
-greet();
-
-
-function greet(name) {
-    console.log("Hello " + name);
-}
-greet("Alice");
-greet("Bob");
-
-
-function greet(name,place) {
-    // console.log("Hello " + name + " from " + place);
-    console.log(`Hello ${name} from ${place}`);
-}
-greet("Alice","New York");
-greet("Bob","London");
+let result = sum(5,10);
+console.log(result);
 
