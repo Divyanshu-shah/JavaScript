34776c1a2ab7c0f945eec2517a393f1b00902c6d
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-17 00:42:35 +0530

    updating

--- file38.js
+++ file38.js
@@ -1,33 +1,57 @@
-// let d = new Date();
+// let d = new Date()
 
-// console.log(d); 
+// console.log(d)
 
-// // get methods
+// //get methods
 
-// console.log(d.getDate()); // getDate returns the day of the month (1-31)
+// console.log(d.getDate())
 
-// console.log(d.getDay());  // getDay returns the day of the week (0-6)
+// console.log(d.getMonth())
 
-// console.log(d.getMonth()); // getMonth returns the month (0-11)
+// console.log(d.getFullYear())
 
-// console.log(d.getFullYear()); // getFullYear returns the year
+// console.log(d.getHours())
 
-// console.log(d.getHours()); // getHours returns the hour (0-23)
+// console.log(d.getMinutes())
 
-// console.log(d.getMinutes()); // getMinutes returns the minutes (0-59)
+// console.log(d.getSeconds())
 
-// console.log(d.getSeconds()); // getSeconds returns the seconds (0-59)
+// console.log(d.getMilliseconds())
 
-// console.log(d.getTime());
+// console.log(d.getDay())
 
-// console.log(d.getMilliseconds()); // getMilliseconds returns the milliseconds (0-999)
+// console.log(d.getTime())
 
-// console.log(Date.now()); // Date.now returns the number of milliseconds since January 1, 1970, 00:00:00 UTC
+// console.log(Date.now())
 
-// let d = new Date(2026,1,1,10,10,10);
-// console.log(d); 
 
-let d = new Date("February 16, 2026 10:10:10");
-console.log(d); 
-// console.log(d.getMonth());
+// let d = new Date(2026,1,1,10,10,10)
+
+// console.log(d)
+
+// let d = new Date("February 16, 2025 10:10:10")
+
+// console.log(d)
+
+// console.log(d.getMonth())
+
+const d = new Date()
+
+// d.setFullYear(2025)
+
+// d.setMonth(11)
+
+// d.setDate(21)
+
+// d.setHours(10)
+
+// d.setMinutes(10)
+
+// d.setSeconds(10)
+
+// d.setMilliseconds(1)
+
+d.setTime(99999900000)
+
+console.log(d)
 
