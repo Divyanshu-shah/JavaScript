42ba2499d778a7bba12dae4b38bb31ff40d0f3ac
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-16 10:17:11 +0530

    updating

--- file33.js
+++ file33.js
@@ -3,10 +3,7 @@ const student = {
     age: 20,    
 }
 
-// const { name, age } = student;
-// console.log(name);
-// console.log(age);
+const { name, age } = student;
+console.log(name);
+console.log(age);
 
-const  {n1,n2}=student;
-console.log(n1);
-console.log(n2);
