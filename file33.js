ed5e12b5a437d7fdcc704bb3cfb480ccc6baa4b3
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-16 10:42:57 +0530

    updating

--- file33.js
+++ file33.js
@@ -1,9 +1,37 @@
+// const student = {
+//     name: 'John Doe',
+//     age: 20,    
+// }
+
+
+// const { name, age } = student;
+// console.log(name);
+// console.log(age);
+
+
+// const {name:userName} = student;
+// console.log(userName);
+
+
+// const student = {
+//     name: 'John Doe',
+//     age: 20,
+//     city: 'jalandhar'
+// }
+// const {city="Amritsar"} = student;
+// console.log(city);
+
+
 const student = {
     name: 'John Doe',
-    age: 20,    
+    age: 20,
+    city: 'jalandhar',
+    marks: {
+        math: 85,
+        science: 90,    
+    }
 }
-
-const { name, age } = student;
-console.log(name);
-console.log(age);
-
+const {marks} = student;
+console.log(marks);
+const {marks:math} = student;
+console.log(math);
