6585acaea344cf53c5d35919400ab1f9ee6361a5
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-17 09:55:40 +0530

    updating

---
+++ file44.js
@@ -0,0 +1,27 @@
+function f1(x) {
+  return new Promise((resolve,reject)=>{     // reject - used to handle errors.
+    setTimeout(()=>{
+        if(x%2==0){
+            // console.log("This is f1 function");
+            resolve(`${x} is an even number`);
+        }else{
+            reject("odd number is not allowed");
+        }
+        resolve();
+    },3000);
+  });
+}
+
+async function main(){
+    let num = Math.round(Math.random()*10);
+    try {
+        const result = await f1(num);
+        console.log(result);
+        console.log("Program completed");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+main();
+
---
+++ file45.js
@@ -0,0 +1,16 @@
+function getStudentInfo() {
+    return {
+        name: "John Doe",
+        email: "john.doe@example.com",
+        age: 20
+    };
+}
+
+async function main() {
+    const result = await getStudentInfo();
+    console.log(result);
+    console.log("Program completed");
+}
+
+main();
+// getStudentInfo() should return name, email and age.
